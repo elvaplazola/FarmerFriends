e73fd2f96c950d5ce90370387e0096462cbafc1b
Dyamez <dyamez@outlook.com>
2023-11-06 09:42:57 -0800

    morning commit

---
+++ frontend/src/Components/Popular/Popular.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Popular.css";
+
+const Popular = () => {
+  return (
+    <div>
+      <div className="popular"></div>
+    </div>
+  );
+};
+
+export default Popular;
