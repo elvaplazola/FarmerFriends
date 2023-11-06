eaab294647e36cac2640d617a65c6159a28e0d8d
Dyamez <dyamez@outlook.com>
2023-11-05 18:52:47 -0800

    navbar draft

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -1,27 +1,50 @@
-import React from "react";
+import { React, useState } from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.jpg";
 import cart_icon from "../Assets/cart_icon.png";
 
 const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="farmer friends logo" />
-        <p>Client</p>
+        {/*<p>Farmer</p> */}
       </div>
       <ul className="nav-menu">
-        <li>
-          Shop <hr />
+        <li
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          Shop{menu === "shop" ? <hr /> : <></>}
         </li>
-        <li>Livestock</li>
-        <li>Produce</li>
-        <li>Supplies</li>
+        <li
+          onClick={() => {
+            setMenu("livestock");
+          }}
+        >
+          Livestock{menu === "livestock" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("produce");
+          }}
+        >
+          Produce{menu === "produce" ? <hr /> : <></>}
+        </li>
+        <li
+          onClick={() => {
+            setMenu("supplies");
+          }}
+        >
+          Supplies{menu === "supplies" ? <hr /> : <></>}
+        </li>
       </ul>
       <div className="nav-login-cart">
         <button>Login</button>
         <img src={cart_icon} alt="cart icon" />
-        <div className="nav-cart-count"></div>
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   );
