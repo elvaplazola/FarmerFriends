62f5095f14b951800869d43954ada81d20ff77e9
Dyamez <dyamez@outlook.com>
2023-11-08 11:14:08 -0800

    add to cart display

--- frontend/src/Pages/Product.jsx
+++ frontend/src/Pages/Product.jsx
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { useContext } from "react";
+import { ShopContext } from "../Context/ShopContext";
+import { useParams } from "react-router-dom";
+import Breadcrumbs from "../Components/Breadcrumbs/Breadcrumbs";
+import ProductDisplay from "../Components/ProductDisplay/ProductDisplay";
 
 const Product = () => {
-  return <div></div>;
+  const { all_product } = useContext(ShopContext);
+  const { productId } = useParams();
+  const product = all_product.find((e) => e.id === Number(productId));
+  return (
+    <div>
+      <Breadcrumbs product={product} />
+      <ProductDisplay product={product} />
+    </div>
+  );
 };
 
 export default Product;
