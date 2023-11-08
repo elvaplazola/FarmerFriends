1ce5b01bf3f833eb05531d3a6c89a2a538f3f827
Dyamez <dyamez@outlook.com>
2023-11-07 21:11:08 -0800

    signup route

--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import "./CSS/LoginSignup.css";
 
 const LoginSignup = () => {
-  return <div></div>;
+  return (
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign-Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Proceed</button>
+        <p className="loginsignup-login">
+          Join us! <span>Login here!</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>
+            Signup terms and policy is governed by Farmer Friends & Team 3
+            policy.
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default LoginSignup;
