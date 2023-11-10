726e9b1d641a2f8379f65d445ba5ca34a673b0ea
Brian Guintos Recinos <brianguintos@gmail.com>
2023-11-09 18:25:23 -0800

    worked on username and password authentication

--- server/server.js
+++ server/server.js
@@ -20,30 +20,29 @@ const pool = new Pool({
     password: '',
     port: 5000,
   });
-  
+
   app.use(bodyParser.json());
- 
+
   // Register endpoint
   app.post('/register', async (req, res) => {
     const { username, password } = req.body;
-  
+
     try {
       // this is where we will check if the username is already taken
       const existingUser = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
       if (existingUser.rows.length > 0) {
         return res.status(400).json({ error: 'Username already taken' });
       }
-  
+
       // hash password
       const hashedPassword = await bcrypt.hash(password, 10);
-  
+
       // insert a new user
       await pool.query('INSERT INTO users (username, password) VALUES ($1, $2)', [username, hashedPassword]);
-  
+
       res.status(201).json({ message: 'User registered successfully' });
     } catch (error) {
       console.error('Error registering user:', error);
       res.status(500).json({ error: 'Internal Server Error' });
     }
   });
-  
