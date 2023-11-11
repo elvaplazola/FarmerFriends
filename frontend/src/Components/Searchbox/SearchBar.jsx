1c86f95b62868eda6109b730adf55284e4bb91cc
Dyamez <dyamez@outlook.com>
2023-11-11 11:35:47 -0800

    Proper ReadMe

--- frontend/src/Components/Searchbox/SearchBar.jsx
+++ frontend/src/Components/Searchbox/SearchBar.jsx
@@ -4,18 +4,17 @@ import all_prod from "../Assets/all_prod.json";
 
 import "./SearchBar.css";
 
-export const SearchBar = ({ setResults }) => {
+const SearchBar = ({ setResults }) => {
   const [input, setInput] = useState("");
 
   const fetchData = (value) => {
-    fetch({ all_prod }) //Backend URL go here!
-      .then((response) => response.json())
-      .then((json) => {
-        const results = json.filter((name, category) => {
-          return value && name && category;
-        });
-        setResults(results);
-      });
+    fetch(`${all_prod}`); //Backend URL go here! //"https://api.jsonserve.com/3fxrEh"
+    //.then((response) => response.json())
+    //.then((json) => {
+    //const results = json.filter((name, category) => {
+    //return name && category;
+    //});
+    //setResults(results);
   };
 
   const handleChange = (value) => {
@@ -36,3 +35,93 @@ export const SearchBar = ({ setResults }) => {
 };
 
 export default SearchBar;
+
+/*
+import React, { useState } from "react";
+import "./SearchBar.css";
+import { FaSearch } from "react-icons/fa";
+import all_prod from "../Assets/all_prod.json";
+
+function SearchBar() {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        type="text"
+        placeholder="Type here to search Farmer Friend..."
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      {all_prod
+        .filter((id) => {
+          if (searchTerm === "") {
+            return id;
+          } else if (id.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return id;
+          }
+        })
+        .map((id, name) => {
+          return <div>{id.name} </div>;
+        })}
+    </div>
+  );
+}
+
+export default SearchBar;
+
+/*
+import React, { useRef } from "react";
+import { Form, FormGroup, Col } from "reactstrap";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchBar() {
+ const dataRef = useRef('');
+ const navigate = useNavigate();
+
+ const searchMe = async () => {
+  const name = dataRef.current.value;
+  
+  }
+    const res = fetch('https://api.jsonserve.com/3fxrEh')
+ };
+
+
+import { useState } from "react";
+import { FaSearch } from "react-icons/fa";
+import all_prod from "../Assets/all_prod.json";
+
+import "./SearchBar.css";
+
+export const SearchBar = ({ setResults }) => {
+  const [input, setInput] = useState("");
+
+  const fetchData = (value) => {
+    fetch(`${all_prod}`); //Backend URL go here! //"https://api.jsonserve.com/3fxrEh"
+    //.then((response) => response.json())
+    //.then((json) => {
+    //const results = json.filter((name, category) => {
+    //return name && category;
+    //});
+    //setResults(results);
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
+  return (
+    <div className="input-wrapper">
+      <FaSearch id="search-icon" />
+      <input
+        placeholder="Type here to search Farmer Friend..."
+        value={input}
+        onChange={(e) => handleChange(e.target.value)}
+      />
+    </div>
+  );
+};
+
+export default SearchBar;
+*/
